d44b59facf29bbeef8808711cd41dc8e835f2b34
Bowab <Bowas92@gmail.com>
2019-05-22 17:40:42 +0200

    Setup

--- CDDA/Startup.cs
+++ CDDA/Startup.cs
@@ -15,6 +15,7 @@ namespace CDDA
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +24,17 @@ namespace CDDA
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=home}/{action=index}/{id?}");
             });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
---
+++ CDDA/gulpfile.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/css.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
